3f376fe8411e1d194350a606c8ebed92aeca5145
vikslap <souveekpal@gmail.com>
2026-04-18 09:44:14 +0530

    Linking code to Vercel environment variables

--- sanity.ts
+++ sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: '24tm4fmj',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '24tm4fmj',
   dataset: 'production',
   apiVersion: '2024-04-18',
   useCdn: false,
